f8264f5b8d0830e1363edf35026f47d3fdc3bdf0
Honningbolden <hmun@itu.dk>
2026-09-04 13:53:46 +0200

    Refactor: Use CsvHelper library to read CSV file
    Co-authored-by:
    Simon <simongskouboe10@gmail.com>,
    Hannibal <hannibalmunch@gmail.com>,
    Filip <filipsejer@gmail.com>,
    Mikkel <mikkel.vh11@gmail.com>
    Daniel <dmkyhl@gmail.com>

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,31 +1,40 @@
-﻿using System.Globalization;
+﻿// using System.Globalization;
+// using System.Runtime.InteropServices;
+// using CsvHelper;
+// using CsvHelper.Configuration;
+
+// Read
+using System.Globalization;
+using CsvHelper;
 
 if (args.Length > 0 && args[0] == "read")
 {
-foreach (var line in File.ReadLines("bison_observe_cli_db.csv").Skip(1))
-{
-    var first = line.IndexOf(',');
-    var last = line.LastIndexOf(',');
+    using (var reader = new StreamReader("bison_observe_cli_db.csv"))
+    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    {
+        var records = csv.GetRecords<Cheep>();
 
-    var author = line[..first];
-    var message = line[(first + 1)..last].Trim('"');
-    var time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(line[(last + 1)..])).LocalDateTime;
+        foreach (var line in records)
+        {
+            Console.WriteLine($"{line.Author} @ {line.Timestamp.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture)}: {line.Observation}");
+        }
+    }
+}
 
-    Console.WriteLine($"{author} @ {time.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture)}: {message}");
-}
-}
+// Write
 if (args.Length > 0 && args[0] == "observe" && args.Length > 1)
 {
     // Handle the observe command for the specific location
     using (StreamWriter stream = File.AppendText("bison_observe_cli_db.csv"))
     {
+
         string author = Environment.UserName;
         string message = args[1];
         long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-       
 
-        stream.WriteLine($"{author},\"{message}\",{timestamp}" );
+
+        stream.WriteLine($"{author},\"{message}\",{timestamp}");
         Console.WriteLine($"Observation recorded for '{message}'.");
     }
-    
+
 }
