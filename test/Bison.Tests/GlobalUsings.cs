b6fdce00d9bcac0e64979236c051edf5d4d18fff
Filip Jacobsen <fija@itu.dk>
2026-09-18 14:43:12 +0200

    [Comment] Explanation to DisableTestParallelization

--- test/Bison.Tests/GlobalUsings.cs
+++ test/Bison.Tests/GlobalUsings.cs
@@ -1,3 +1,9 @@
 global using Xunit;
 
+/**
+    * This fixes the bug where xUnit tests are run in parallel,
+    * which can cause issues with shared resources like files or databases.
+    * By disabling test parallelization, we ensure that tests run sequentially, 
+    * preventing potential conflicts and ensuring consistent test results.
+*/
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
