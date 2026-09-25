2aa8dff25e76491c0e6030cc4784e9060a39be4b
Simon Skouboe <sgsk@itu.dk>
2026-09-25 15:04:17 +0200

    Refactor: Make our program use SQLite-backed observations
    Co-Authored-By: Filip Sejer Jacobsen <filipsejer@gmail.com>

--- src/Bison.Razor/Program.cs
+++ src/Bison.Razor/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var dbPath = Environment.GetEnvironmentVariable("BISONDBPATH") ?? Path.Combine(Path.GetTempPath(), "bison.db");
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton(new DBFacade(dbPath));
 builder.Services.AddSingleton<IObservationService, ObservationService>();
-
 
 var app = builder.Build();
 
@@ -11,7 +13,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
